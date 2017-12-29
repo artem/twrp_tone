375e3a46e244a9d2ec2f2999d25b8df0f9215ff7
Artem Labazov <123321artyom@gmail.com>
2017-12-29 20:46:51 +0300

    Fix tzdata on android 8.1

--- omni_kagura.mk
+++ omni_kagura.mk
@@ -19,10 +19,6 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-
 # Device identification
 PRODUCT_DEVICE := kagura
 PRODUCT_NAME := omni_kagura
